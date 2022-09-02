50249fc036027569349479cf156c3efd2586109e
PopovaElis <elizprest@gmail.com>
2022-09-02 21:07:03 +0500

    2 отдельных класса

---
+++ src/Book.java
@@ -0,0 +1,19 @@
+public class Book {
+
+    String nameOfTheBook;
+    int publicationYear;
+
+public Book(String nameOfTheBook, int publicationYear){
+        this.nameOfTheBook=nameOfTheBook;
+        this.publicationYear =publicationYear;
+    }
+    public String getNameOfTheBook(){
+        return this.nameOfTheBook;
+    }
+    public int getPublicationYear(){
+        return this.publicationYear;
+}
+}
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args){
+        Book dorianGray  = new Book("The picture of Dorian Gray", 1890);
+        Book winnieThePooh = new Book("Winnie-The-Pooh",1926);
+        Author oscarWilde = new Author("Oscar","Wilde");
+        Author alanMilne = new Author("Alan","Milne");
+
+        System.out.println ("Название книги '" + dorianGray.getNameOfTheBook()+"'");
+        System.out.println ("Год издания " + dorianGray.getPublicationYear());
+        System.out.println ("Автор книги " +  oscarWilde.getName()+" "+oscarWilde.getSurname());
     }
-}
+    }
